a8c14f78287417b584b5818680b00d733935e1b9
Helmut Steiner <helmut.steiner@student.uibk.ac.at>
2015-07-25 15:10:00 +0200

    Added credential settings.

--- webroot/js/variables.js
+++ webroot/js/variables.js
@@ -2,6 +2,6 @@
  * 
  */
 
-var AUTH0_CLIENT_ID='YOUR_CLIENT_ID'; 
-var AUTH0_DOMAIN='YOUR_NAMESPACE'; 
+var AUTH0_CLIENT_ID='R3Xvq6jPmaI6b712OH3D0IERdtaveKXE'; 
+var AUTH0_DOMAIN='mypredectivefarm.eu.auth0.com'; 
 var AUTH0_CALLBACK_URL=location.href;
