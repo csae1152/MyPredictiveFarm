59556c1feca8238794cd061aae35628497bc46f1
Helmut Steiner <helmut.steiner@student.uibk.ac.at>
2015-07-25 13:47:58 +0200

    Added new source for javascript.

---
+++ webroot/js/login.js
@@ -0,0 +1,62 @@
+/**
+ * @author: oAuth from internet.
+ */
+
+$(document).ready(function() {
+			    var lock = new Auth0Lock(
+			      // All these properties are set in auth0-variables.js
+			    		var AUTH0_CLIENT_ID='R3Xvq6jPmaI6b712OH3D0IERdtaveKXE'; 
+			    		var AUTH0_DOMAIN='mypredectivefarm.eu.auth0.com'; 
+			    		var AUTH0_CALLBACK_URL=location.href;
+			    );
+
+			    var userProfile;
+
+			    $('.btn-login').click(function(e) {
+			      e.preventDefault();
+			      lock.showSignin(function(err, profile, token) {
+			        if (err) {
+			          // Error callback
+			          console.log("There was an error");
+			          alert("There was an error logging in");
+			        } else {
+			          // Success calback
+
+			          // Save the JWT token.
+			          localStorage.setItem('userToken', token);
+
+			          // Save the profile
+			          userProfile = profile;
+
+			          $('.login-box').hide();
+			          $('.logged-in-box').show();
+			          $('.nickname').text(profile.nickname);
+			          $('.nickname').text(profile.name);
+			          $('.avatar').attr('src', profile.picture);
+			        }
+			      });
+			    });
+
+			    $.ajaxSetup({
+			      'beforeSend': function(xhr) {
+			        if (localStorage.getItem('userToken')) {
+			          xhr.setRequestHeader('Authorization',
+			                'Bearer ' + localStorage.getItem('userToken'));
+			        }
+			      }
+			    });
+
+			    $('.btn-api').click(function(e) {
+			      // Just call your API here. The header will be sent
+			      $.ajax({
+			        url: 'http://localhost:3001/secured/ping',
+			        method: 'GET'
+			      }).then(function(data, textStatus, jqXHR) {
+			        alert("The request to the secured enpoint was successfull");
+			      }, function() {
+			        alert("You need to download the server seed and start it to call this API");
+			      });
+			    });
+
+
+			});
