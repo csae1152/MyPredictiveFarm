37b13500e46cd2b1310cca91c325e951486e1b9d
Helmut Steiner <csae1152@uibk.ac.at>
2015-11-28 15:18:14 +0100

    Update easing.js

--- webroot/web/js/easing.js
+++ webroot/web/js/easing.js
@@ -19,6 +19,10 @@ jQuery.extend( jQuery.easing,
 	easeOutQuad: function (x, t, b, c, d) {
 		return -c *(t/=d)*(t-2) + b;
 	},
+	
+	easeIsoOut: function(x, y) {
+		return -a*b;	
+	},
 	easeInOutQuad: function (x, t, b, c, d) {
 		if ((t/=d/2) < 1) return c/2*t*t + b;
 		return -c/2 * ((--t)*(t-2) - 1) + b;
