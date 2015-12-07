5720c68eb7324f4d485babe4e8a9a516e1fc698f
Helmut Steiner <csae1152@uibk.ac.at>
2015-12-07 09:36:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var ip = container.env['OPENSHIFT_VERTX_IP'] || '127.0.0.1';
 var port = parseInt(container.env['OPENSHIFT_VERTX_PORT'] || 8000);
 
 vertx.createHttpServer().requestHandler(function(req) {
-  var file = req.path() === '/' ? 'index_dashboard.html' : req.path();
+  var file = req.path() === '/' ? 'index.html' : req.path();
   req.response.sendFile('webroot/' + file);
 }).listen(port, ip, function(err) {
     if (!err) {
